f07b4860f5790179b49298e6f325ba355b8a76c1
Carl <cjones6343@gmail.com>
2019-01-13 20:43:47 -0500

    Content Updates

--- gberries-shop/resources/views/welcome.blade.php
+++ gberries-shop/resources/views/welcome.blade.php
@@ -4,12 +4,15 @@
     Gourmet Berries Shopping Cart
 @endsection
 @section('content')
+    <!--Row 1-->
+    <br>
     <div class="card-deck">
         <div class="card">
             <img class="card-img-top" src="https://scontent-ort2-1.cdninstagram.com/vp/6b27a894e1aa278928b8b91d938e3008/5CD0D927/t51.2885-15/e35/27581967_406399839804254_4780757320135081984_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">White Chocolate Strawberries</h5>
-                <p class="card-text">Spread the Love ❤ With all white chocolate dipped strawberries. </p>
+                <p class="card-text">Spread the love ❤ with a Gourmet Berries classic! All white chocolate dipped & painted strawberries. </p>
+                <div class="" style="text-align: right;"><p>Stating at $12.00 USD</p></div>
                 <button type="button" class="btn btn-primary">Add to cart</button>
 
             </div>
@@ -21,7 +24,8 @@
             <img class="card-img-top" src="https://scontent-ort2-1.cdninstagram.com/vp/74243115eaa285e3386ee383b81aaff9/5CB355EC/t51.2885-15/sh0.08/e35/s750x750/27577054_332021363961731_1418660073332277248_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">Caramel Apples</h5>
-                <p class="card-text">Apples dipped in Plain Caramel, Carmel with Pecans, Carmel with Peanuts, Butterfinger, Snickers & Oreo.</p>
+                <p class="card-text">Apples dipped in Plain Caramel, Carmel with Pecans, Carmel with Peanuts, Butterfinger, or Snickers & Oreo.</p>
+                <div class="" style="text-align: right;"><p>$11.00 USD</p></div>
                 <button type="button" class="btn btn-primary">Add to cart</button>
 
             </div>
@@ -35,6 +39,7 @@
             <div class="card-body">
                 <h5 class="card-title">Pecan Turtles</h5>
                 <p class="card-text">Homemade Pecan Turtles! Fresh Buttery Carmel with Pecans covered in Milk Chocolate 😋</p>
+                <div class="" style="text-align: right;"><p>Stating at $12.00 USD</p></div>
                 <button type="button" class="btn btn-primary">Add to cart</button>
             </div>
             <div class="card-footer">
@@ -42,12 +47,15 @@
             </div>
         </div>
     </div>
+    <!--Row 2-->
+    <br>
     <div class="card-deck">
         <div class="card">
             <img class="card-img-top" src="https://scontent-ort2-1.cdninstagram.com/vp/74cc27b42235555701aa71ecf73023e3/5CCAED3A/t51.2885-15/sh0.08/e35/s640x640/26342105_1768960509816004_1507080741148688384_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">Pink & White Twist Tray</h5>
-                <p class="card-text">Pink & White painted chocolate dipped strawberries with pretzels.</p>
+                <h5 class="card-title">"Pink & White Twist" Tray</h5>
+                <p class="card-text">Pink & White painted chocolate dipped strawberries with dipped and painted pretzels.</p>
+                <div class="" style="text-align: right;"><p>$35.00 USD</p></div>
                 <button type="button" class="btn btn-primary">Add to cart</button>
             </div>
             <div class="card-footer">
@@ -59,6 +67,7 @@
             <div class="card-body">
                 <h5 class="card-title">Custom Treat Boxes</h5>
                 <p class="card-text">This box can be customized however you like, with your choice of liquor.</p>
+                <div class="" style="text-align: right;"><p>$50.00 USD</p></div>
                 <button type="button" class="btn btn-primary">Add to cart</button>
             </div>
             <div class="card-footer">
@@ -71,6 +80,48 @@
             <div class="card-body">
                 <h5 class="card-title">Platnium Box</h5>
                 <p class="card-text">The Platnium Package. One dozen roses, one dozen strawberries, and a bottle of wine. One of the strawberries is strawberry crunch!</p>
+                <div class="" style="text-align: right;"><p>$75.00 USD</p></div>
+                <button type="button" class="btn btn-primary">Add to cart</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+    </div>
+    <!--Row 3-->
+    <br>
+    <div class="card-deck">
+        <div class="card">
+            <img class="card-img-top" src="https://scontent-ort2-1.cdninstagram.com/vp/e7eb67534920589e22476c46679a39d9/5CB9D6D8/t51.2885-15/sh0.08/e35/s750x750/22499961_286303678546903_3459540507036745728_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">"Glitz & Glam" Party Trays</h5>
+                <p class="card-text">Party tray of Royal Blue or Golden “Glitz & Glam” Chocolate dipped Strawberries.</p>
+                <div class="" style="text-align: right;"><p>$55.00 USD</p></div>
+                <button type="button" class="btn btn-primary">Add to cart</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="https://scontent-ort2-1.cdninstagram.com/vp/082ccb1400f36c5d9919f393ab04f6e6/5CD4C85B/t51.2885-15/sh0.08/e35/s640x640/42161333_502492363553375_6467766653434791603_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">"Glitz & Glam" Infused Party Trays</h5>
+                <p class="card-text">Party tray of Diamond White Chocolate “Glitz & Glam” Berries infused with your liquor of choice (Hennessy used in photo).</p>
+                <div class="" style="text-align: right;"><p>$75.00 USD</p></div>
+                <button type="button" class="btn btn-primary">Add to cart</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top"
+                 src="https://scontent-ort2-1.cdninstagram.com/vp/19f40f738b52cb7d4987b5ba77e2a612/5CB691AB/t51.2885-15/sh0.08/e35/s750x750/25018912_291189308071011_4925020779071930368_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">"Double Chocolate" Party Tray</h5>
+                <p class="card-text">Party Tray of our signature Carmel Apples & Homemade Pecan Turtles!</p>
+                <div class="" style="text-align: right;"><p>$55.00 USD</p></div>
                 <button type="button" class="btn btn-primary">Add to cart</button>
             </div>
             <div class="card-footer">
