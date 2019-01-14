6b21a1c51aa0b990d77f4a3d3176e5bb13413d85
Carl <cjones6343@gmail.com>
2019-01-13 20:03:00 -0500

    Beggining of market content layout

--- gberries-shop/resources/views/welcome.blade.php
+++ gberries-shop/resources/views/welcome.blade.php
@@ -4,5 +4,78 @@
     Gourmet Berries Shopping Cart
 @endsection
 @section('content')
-    <h1>This view works!</h1>
+    <div class="card-deck">
+        <div class="card">
+            <img class="card-img-top" src="https://scontent-ort2-1.cdninstagram.com/vp/6b27a894e1aa278928b8b91d938e3008/5CD0D927/t51.2885-15/e35/27581967_406399839804254_4780757320135081984_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">White Chocolate Strawberries</h5>
+                <p class="card-text">Spread the Love ❤ With all white chocolate dipped strawberries. </p>
+                <button type="button" class="btn btn-primary">Add to cart</button>
+
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="https://scontent-ort2-1.cdninstagram.com/vp/74243115eaa285e3386ee383b81aaff9/5CB355EC/t51.2885-15/sh0.08/e35/s750x750/27577054_332021363961731_1418660073332277248_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Caramel Apples</h5>
+                <p class="card-text">Apples dipped in Plain Caramel, Carmel with Pecans, Carmel with Peanuts, Butterfinger, Snickers & Oreo.</p>
+                <button type="button" class="btn btn-primary">Add to cart</button>
+
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top"
+                 src="https://scontent-ort2-1.cdninstagram.com/vp/46e0d664de36445b3a074877563b0082/5CC3C8DF/t51.2885-15/sh0.08/e35/s750x750/27878211_179832972780671_2588390172970188800_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Pecan Turtles</h5>
+                <p class="card-text">Homemade Pecan Turtles! Fresh Buttery Carmel with Pecans covered in Milk Chocolate 😋</p>
+                <button type="button" class="btn btn-primary">Add to cart</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+    </div>
+    <div class="card-deck">
+        <div class="card">
+            <img class="card-img-top" src="https://scontent-ort2-1.cdninstagram.com/vp/74cc27b42235555701aa71ecf73023e3/5CCAED3A/t51.2885-15/sh0.08/e35/s640x640/26342105_1768960509816004_1507080741148688384_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Pink & White Twist Tray</h5>
+                <p class="card-text">Pink & White painted chocolate dipped strawberries with pretzels.</p>
+                <button type="button" class="btn btn-primary">Add to cart</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="https://scontent-ort2-1.cdninstagram.com/vp/55fc2f788aef35742535e269f020f957/5CD63131/t51.2885-15/sh0.08/e35/s750x750/42812077_1973996062666439_2195486384893759971_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Custom Treat Boxes</h5>
+                <p class="card-text">This box can be customized however you like, with your choice of liquor.</p>
+                <button type="button" class="btn btn-primary">Add to cart</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top"
+                 src="https://scontent-ort2-1.cdninstagram.com/vp/5bcbc61e02f9c7e7c8028823b3dd6f09/5CC2110B/t51.2885-15/sh0.08/e35/p750x750/31556658_225987158164100_7955580815872622592_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Platnium Box</h5>
+                <p class="card-text">The Platnium Package. One dozen roses, one dozen strawberries, and a bottle of wine. One of the strawberries is strawberry crunch!</p>
+                <button type="button" class="btn btn-primary">Add to cart</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+    </div>
 @endsection
